edf637f4b88614a07936132368a35085537a7633
Pablo Valdez <valdeznet@gmail.com>
2023-05-08 13:08:26 -0300

    Delegacion

--- Controlinventarioissn/Controlinventarioissn/Migrations/20230508115604_uno.Designer.cs
+++ Controlinventarioissn/Controlinventarioissn/Migrations/20230508115604_uno.Designer.cs
@@ -3,6 +3,7 @@ using Controlinventarioissn.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Controlinventarioissn.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230508115604_uno")]
+    partial class uno
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
